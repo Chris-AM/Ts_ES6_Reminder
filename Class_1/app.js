72b5a19c9c00fa2e4716acfce7bf72cede058de5
Chris-AM <c.aranguizm@outlook.com>
2020-11-24 18:10:38 -0300

    class 1

--- Class_1/app.js
+++ Class_1/app.js
@@ -1,9 +1,14 @@
+"use strict";
 (function () {
     function sayHello(name) {
         console.log('Hi' + ' ' + name);
+    }
+    function sayGoodBye(name) {
+        console.log('Goodbye' + ' ' + name);
     }
     var WOLVERINE = {
         name: 'Logan'
     };
     sayHello(WOLVERINE.name);
+    sayGoodBye(WOLVERINE.name);
 });
--- Class_1/app.ts
+++ Class_1/app.ts
@@ -2,10 +2,16 @@
     function sayHello(name : string) {
         console.log('Hi' + ' ' + name);
     }
+
+    function sayGoodBye(name: string) {
+        console.log('Goodbye' + ' ' + name);
+    }
     
     const WOLVERINE = {
         name: 'Logan'
     };
     
     sayHello(WOLVERINE.name);
+
+    sayGoodBye(WOLVERINE.name);
 })
