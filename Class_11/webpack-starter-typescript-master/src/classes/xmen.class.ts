39f6bb8e3e2fd514997a9335b0d7007d3a6363c4
Chris-AM <c.aranguizm@outlook.com>
2020-11-26 14:40:12 -0300

    decorators

--- Class_11/webpack-starter-typescript-master/src/classes/xmen.class.ts
+++ Class_11/webpack-starter-typescript-master/src/classes/xmen.class.ts
@@ -1,3 +1,7 @@
+function printConsole(classConstructor: Function) {
+  console.log(classConstructor);
+}
+@printConsole
 export class Xmen{
   constructor(
     public name: string,
--- Class_11/webpack-starter-typescript-master/src/index.ts
+++ Class_11/webpack-starter-typescript-master/src/index.ts
@@ -2,4 +2,4 @@ import { Xmen } from './classes/xmen.class';
 
 const WOLVERINE = new Xmen('Logan', 'Wolverine');
 
-WOLVERINE.print();
+//WOLVERINE.print();
